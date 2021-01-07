9a91948a3df41a1d41b860f83739573e88982608
Marinko <marinkospasojevic@gmail.com>
2021-01-07 19:29:45 +0100

    Query strings in Blazor WebAssembly source code.

---
+++ QueryStringExample/QueryStringExample/Extensions/NavigationManagerExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace QueryStringExample.Extensions
+{
+	public static class NavigationManagerExtension
+	{
+		public static T ExtractQueryStringByKey<T>(this NavigationManager navManager, string key)
+		{
+			var uri = navManager.ToAbsoluteUri(navManager.Uri);
+			QueryHelpers.ParseQuery(uri.Query)
+				.TryGetValue(key, out var queryValue);
+
+			if (typeof(T).Equals(typeof(int)))
+			{
+				int.TryParse(queryValue, out int result);
+				return (T)(object)result;
+			}
+
+			if (typeof(T).Equals(typeof(string)))
+				return (T)(object)queryValue.ToString();
+
+			return default;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# blazor-wasm-query-strings
-This repo contains the source code for the "Query Strings in Blazor WebAssembly" article on Code Maze
+# How to Use Query Strings in Blazor WebAssembly
+## https://code-maze.com/query-strings-blazor-webassembly
+This repo contains the source code for the "How to Use Query Strings in Blazor WebAssembly" article on Code Maze
